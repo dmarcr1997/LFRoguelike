2869bb560eaf8ab8e91c44d87afad5c13bcbb9fc
Damon Rocha <dmarcr1997@gmail.com>
2023-12-09 01:36:21 -0600

    Added game assets

--- Source/NeonLFSam/NeonLFCharacter.cpp
+++ Source/NeonLFSam/NeonLFCharacter.cpp
@@ -23,6 +23,16 @@ void ANeonLFCharacter::BeginPlay()
 	
 }
 
+void ANeonLFCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
+void ANeonLFCharacter::MoveRight(float value)
+{
+	AddMovementInput(GetActorRightVector(), value);
+}
+
 // Called every frame
 void ANeonLFCharacter::Tick(float DeltaTime)
 {
@@ -34,6 +44,9 @@ void ANeonLFCharacter::Tick(float DeltaTime)
 void ANeonLFCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward",this, &ANeonLFCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ANeonLFCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
 
 }
 
